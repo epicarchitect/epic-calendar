a1480f9cac5645563ca2995db85095f3de4b201c
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-05-03 23:46:14 +0700

    little save

--- sample/shared/src/iosMain/kotlin/main.ios.kt
+++ sample/shared/src/iosMain/kotlin/main.ios.kt
@@ -2,4 +2,5 @@ import androidx.compose.ui.window.ComposeUIViewController
 
 actual fun getPlatformName(): String = "iOS"
 
+@Suppress("FunctionName", "unused")
 fun MainViewController() = ComposeUIViewController { App() }
